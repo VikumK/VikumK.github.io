3b5c1917fa134c39a61fcddf74749ad692cc5d6e
VikumK <vikumbk@outlook.com>
2024-02-17 17:59:51 +0530

    backto normal

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,20 +1,6 @@
-document
-  .getElementById("urlShortenerForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+let fullName = "Jone";
+let age = 20;
+let student = true;
 
-    var longUrl = document.getElementById("longUrlInput").value;
-
-    // Here you would implement logic to generate a short URL and save the mapping
-    var shortUrl = generateShortUrl(longUrl);
-
-    document.getElementById("shortenedUrl").innerText =
-      "Shortened URL: " + shortUrl;
-  });
-
-function generateShortUrl(longUrl) {
-  // Here you would implement logic to generate a short URL
-  // This could involve generating a unique ID or using a hashing algorithm
-  // For simplicity, let's assume we just append a random string to the base URL
-  return "https://vikumk.github.io/" + Math.random().toString(36).substr(2, 5);
-}
+document.getElementById("p1").textContent = `My name is ${fullName}`;
+document.getElementById("p2").textContent = `My age is ${age}`;
